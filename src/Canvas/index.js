bd7482d81788d8d142f5fdee9360a0061cdeba63
sojib <mdsojibhosen444@gmail.com>
2023-08-06 09:49:33 +0600

    Commit

---
+++ src/Canvas/index.js
@@ -0,0 +1,12 @@
+console.log("HI");
+
+const element = document.querySelector("canvas")
+const contex = element.getContext("2d");
+contex.fillStyle = "hotpink";
+
+function draw(x,y)
+{
+    
+}
+
+console.log(contex);
