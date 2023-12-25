28df2a22eecdf65dc42798c963713afa5bcdf8a1
sojib <mdsojibhosen444@gmail.com>
2023-12-25 11:49:07 +0600

    Pratice

---
+++ src/Canvas/JavaScript/Truethy and Falsy Value/Index.js
@@ -0,0 +1,25 @@
+
+
+let name = undefined | 0 | null | NaN | " ";
+
+if(name)
+{
+    console.log("Name is defined")
+}
+else
+{
+    console.log("name is not defined");
+}
+//name is not defined
+
+name = "sojib";
+
+if(name)
+{
+    console.log("Name is defined")
+}
+else
+{
+    console.log("name is not defined");
+}
+//name is  defined
---
+++ src/Canvas/JavaScript/Type Conversion/index.js
@@ -0,0 +1,15 @@
+
+
+
+let a = "2"+3;
+console.log(a); // type coercion
+//if you add string here with a number with '+' operator then it will convert number to string the total expression
+//because the string has only the concatination method.
+
+let number = 4324;
+
+let Name = String(number); // type conversion
+console.log(typeof Name);
+
+let con_Number = Number(Name);// type conversion
+console.log(typeof con_Number);
